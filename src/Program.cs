f83962c69a8d8d43a06316a76f1c408aaf9689e6
Subhajit Sahu <wolfram77@gmail.com>
2016-09-25 16:34:51 +0530

    program: dev & test :dog:

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace orez.oecho {
 	class Program {
+
+		// static method
+		/// <summary>
+		/// One of the most vital ingredients to keep a
+		/// relationship going is a capacity for humour.
+		/// : The School of Life
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			oParams p = new oParams(args);
+			string v = p.Value;
+			if (p.Expand) v = Unescape(v);
+			if (!p.NoNewline) v += '\n';
+			Console.Write(v);
+		}
+
+		/// <summary>
+		/// Unescape a C-escaped string.
+		/// </summary>
+		/// <param name="v">Escaped string.</param>
+		/// <returns>Unescaped string.</returns>
+		private static string Unescape(string v) {
+			return Regex.Unescape(v);
 		}
 	}
 }
---
+++ src/oParams.cs
@@ -0,0 +1,46 @@
+﻿namespace orez.oecho {
+	class oParams {
+
+		// data
+		/// <summary>
+		/// Indicates whether escape sequences are to be expanded.
+		/// </summary>
+		public bool Expand;
+		/// <summary>
+		/// Indicates if there is supposed to be no newline at end.
+		/// </summary>
+		public bool NoNewline;
+		/// <summary>
+		/// Stores the actual value to be written.
+		/// </summary>
+		public string Value = "";
+
+
+		// constructor
+		/// <summary>
+		/// Get parameters for input arguments.
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
+		public oParams(string[] args) {
+			for (int i = 0; i < args.Length; i++) {
+				string a = args[i];
+				if (a.StartsWith("--")) { SetOption(a.Substring(2, 1)); continue; }
+				if (a.StartsWith("-")) { SetOption(a.Substring(1)); continue; }
+				Value += a;
+			}
+		}
+
+
+		// method
+		/// <summary>
+		/// Set options from option string.
+		/// </summary>
+		/// <param name="o">Option string.</param>
+		public void SetOption(string o) {
+			foreach(char c in o.ToLower()) {
+				if (c == 'e') Expand = true;
+				else if (c == 'n') NoNewline = true;
+			}
+		}
+	}
+}
